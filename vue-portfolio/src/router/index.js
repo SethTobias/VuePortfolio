dc6d3e5a850b300d4034d05f62a48b910860ceb6
SethTobias <seth.addams1107@gmail.com>
2024-01-11 09:00:16 +0200

    Initail Setup of Views and Components

--- vue-portfolio/src/router/index.js
+++ vue-portfolio/src/router/index.js
@@ -1,25 +1,71 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
+import MusicView from "../views/MusicView.vue";
+import MoviesView from "../views/MoviesView.vue";
+import SportView from "../views/SportView.vue";
+import ReadingView from "../views/ReadingView.vue";
+import ExperienceView from "../views/ExperienceView.vue";
+import ProjectsView from "../views/ProjectsView.vue";
+import TestimonialView from "../views/TestimonialView.vue";
+import ContactView from "../views/ContactView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: "/about",
+    name: "about",
+    component: AboutView,
+  },
+  {
+    path: "/music",
+    name: "music",
+    component: MusicView,
+  },
+  {
+    path: "/movies",
+    name: "movies",
+    component: MoviesView,
+  },
+  {
+    path: "/sport",
+    name: "sport",
+    component: SportView,
+  },
+  {
+    path: "/reading",
+    name: "reading",
+    component: ReadingView,
+  },
+  {
+    path: "/experience",
+    name: "experience",
+    component: ExperienceView,
+  },
+  {
+    path: "/projects",
+    name: "projects",
+    component: ProjectsView,
+  },
+  {
+    path: "/testimonial",
+    name: "testimonial",
+    component: TestimonialView,
+  },
+  {
+    path: "/contact",
+    name: "contact",
+    component: ContactView,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
